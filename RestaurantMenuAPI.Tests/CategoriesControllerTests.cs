35da1f705574428b358c86d7a3b0eb6ea4777f91
Ana <Ana@ANCI>
2026-07-11 20:31:24 +0200

    unit tests

---
+++ RestaurantMenuAPI.Tests/CategoriesControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantMenuAPI.Controllers;
+using RestaurantMenuAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace RestaurantMenuAPI.Tests
+{
+    public class CategoriesControllerTests
+    {
+        private MenuDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<MenuDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            return new MenuDbContext(options);
+        }
+
+        [Fact]
+        public async Task GetCategories_Returns_All_Categories_From_Database()
+        {
+            using var context = GetInMemoryDbContext();
+            context.Categories.AddRange(
+                new Category { Id = 1, Name = "Main Courses" },
+                new Category { Id = 2, Name = "Pizza" }
+            );
+            await context.SaveChangesAsync();
+
+            var controller = new CategoriesController(context);
+
+            var result = await controller.GetCategories();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var categoryList = Assert.IsAssignableFrom<IEnumerable<Category>>(okResult.Value);
+            Assert.Equal(2, categoryList.Count());
+        }
+    }
+}
---
+++ RestaurantMenuAPI.Tests/MenuItemsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantMenuAPI.Controllers;
+using RestaurantMenuAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace RestaurantMenuAPI.Tests
+{
+    public class MenuItemsControllerTests
+    {
+        private MenuDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<MenuDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            return new MenuDbContext(options);
+        }
+
+        [Fact]
+        public async Task GetMenuItems_Returns_Empty_List_When_Menu_Is_Empty()
+        {
+            using var context = GetInMemoryDbContext();
+            var controller = new MenuItemsController(context);
+
+            var result = await controller.GetMenuItems();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var menuItemList = Assert.IsAssignableFrom<IEnumerable<MenuItem>>(okResult.Value);
+            Assert.Empty(menuItemList);
+        }
+    }
+}
